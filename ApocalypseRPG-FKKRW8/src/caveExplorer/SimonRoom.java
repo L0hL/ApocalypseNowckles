ba370507343a50551c95ee98b70b29bd42b6f370
student 8 <student 8@10.8.33.157>
2016-11-10 14:04:06 -0500

    progress(sort of)

---
+++ ApocalypseRPG-FKKRW8/src/caveExplorer/SimonRoom.java
@@ -0,0 +1,14 @@
+package caveExplorer;
+
+public class SimonRoom {
+	public static String matchArray[][];
+	
+	public static String matchBoard = new matchArray[4][5]; 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+	private void makeGame(){
+	
+	}
+}
