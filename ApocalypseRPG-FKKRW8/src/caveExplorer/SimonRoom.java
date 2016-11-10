1106347c3c68713c398eb88a0e81f8fd60f1c3e0
2MSBHVE <max@miku.nyc>
2016-11-10 17:20:33 -0500

    Commented out error in SimonRoom to avoid error message on every run

--- ApocalypseRPG-FKKRW8/src/caveExplorer/SimonRoom.java
+++ ApocalypseRPG-FKKRW8/src/caveExplorer/SimonRoom.java
@@ -3,7 +3,7 @@ package caveExplorer;
 public class SimonRoom {
 	public static String matchArray[][];
 	
-	public static String matchBoard = new matchArray[4][5]; 
+//	public static String matchBoard = new matchArray[4][5]; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
